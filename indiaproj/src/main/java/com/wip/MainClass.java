9fd47e14b5d084dc00fad8126cebde673e9ffbfb
Lakshita kajla <Jan1user451@rpsconsulting.in>
2026-01-20 17:47:52 +0530

    Updated

--- indiaproj/src/main/java/com/wip/MainClass.java
+++ indiaproj/src/main/java/com/wip/MainClass.java
@@ -15,7 +15,7 @@ public class MainClass {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Java within Kubernetes K8s-- Maven Project!!!");
+		System.out.println("Java within Kubernetes K8s-- Jenkins added Maven Project!!!");
 
 	}
 
